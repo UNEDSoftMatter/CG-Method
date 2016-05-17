41c50363867b94f31f96049690fd2271d597f184
Jaime Arturo de la Torre <jatorre@fisfun.uned.es>
2016-05-17 11:40:12 +0200

    Added deleted file aux.c

---
+++ src/aux.c
@@ -0,0 +1,52 @@
+/*
+ * Filename   : aux.c
+ *
+ * Created    : 25.04.2016
+ *
+ * Modified   : lun 25 abr 2016 13:01:48 CEST
+ *
+ * Author     : jatorre
+ *
+ * Purpose    : Auxiliar functions 
+ *
+ */
+
+#include "cg.h"
+
+double MaxVector (gsl_vector * v)
+{
+  double max = 0.0;
+
+  for (int i=0;i<v->size;i++)
+    (gsl_vector_get(v,i) > max ) ? max = gsl_vector_get(v,i) : max ;
+
+  return max;
+}
+
+double MinVector (gsl_vector * v)
+{
+  double min = 0.0;
+
+  for (int i=0;i<v->size;i++)
+    (gsl_vector_get(v,i) < min ) ? min = gsl_vector_get(v,i) : min ;
+
+  return min;
+}
+
+gsl_vector * RescaleVector (gsl_vector * v)
+{
+  gsl_vector * vrescaled = gsl_vector_calloc (v->size);
+
+  double max = MaxVector(v);
+  double min = MinVector(v);
+  double vi;
+
+  for (int i=0;i<v->size;i++)
+  {
+    vi = (gsl_vector_get(v,i)-min)/(max-min);
+    gsl_vector_set(vrescaled,i,vi);
+  }
+
+  return vrescaled; 
+
+}
